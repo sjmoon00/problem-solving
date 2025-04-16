0cf4d260a398c99b2edd291b272658d6784306ea
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2025-04-16 17:38:39 +0900

    [level 2] Title: 피로도, Time: 50.72 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,33 +1,18 @@
+from itertools import permutations
 def solution(k, dungeons):
     answer = -1
     N = len(dungeons)
-    used = [False] * N
-
-    def permutation(result, used):
-        nonlocal answer
-        if len(result) == N:
-            cnt = calc(result)
-            answer = max(answer, cnt)
-
-        for i in range(N):
-            if used[i]:
-                continue
-            result.append(dungeons[i])
-            used[i] = True
-            permutation(result, used)
-            used[i] = False
-            result.pop()
-
-    def calc(result):
+    perms = permutations(dungeons, N)
+    
+    for perm in perms:
         my_k = k
         cnt = 0
-        for require, consume in result:
+        for require, consume in perm:
             if my_k < require:
                 continue
             
             my_k -= consume
             cnt += 1
-        return cnt
+        answer = max(answer, cnt)
     
-    permutation([], used)
     return answer

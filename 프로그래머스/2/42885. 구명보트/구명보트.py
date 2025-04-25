f964694af1540ea24f38a8e3808b845bce607e51
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2025-04-25 23:43:55 +0900

    [level 2] Title: 구명보트, Time: 9.98 ms, Memory: 9.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+
+    l, r = 0, len(people) - 1
+    while l < r:
+        if people[l] + people[r] <= limit:
+            l += 1
+            r -= 1
+        else:
+            r -= 1
+        answer += 1        
+    if l == r:
+        answer += 1
+    return answer

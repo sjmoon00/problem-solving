4fab556e9d062699815db10764fb08bcd317e528
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2025-05-21 17:11:18 +0900

    Time: 55 ms (60.22%), Space: 21.9 MB (47.4%) - LeetHub

---
+++ LeetCode/1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        dic = defaultdict(list)
+        for product in products:
+            x = ''
+            for p in product:
+                x += p
+                dic[x].append(product)
+        
+        answer = []
+        x = ''
+        for c in searchWord:
+            x += c
+            candid = list(sorted(dic[x]))
+            answer.append(candid[:3])
+        return answer

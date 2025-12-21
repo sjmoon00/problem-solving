566a1e650dc0418d593ef956a1c94b83d56dbf28
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2025-12-21 11:55:41 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -2,11 +2,10 @@ def solution(n):
     answer = 0
     
     tri = ''
-    while n >= 3:
+    while n:
         div, mod = divmod(n, 3)
         tri += str(mod)
         n = div
-    tri += str(n)
 
     answer = int(tri, 3)
     return answer

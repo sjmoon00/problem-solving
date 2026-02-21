043a2cdbeb9e16db2483087d590d0ed9d6c09307
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-02-21 14:45:51 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 102.85 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.py
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.py
@@ -1,13 +1,14 @@
-def calc_degrees(t):
-    h_degree = (t % (12*60*60)) * (360 / (12*60*60))
-    m_degree = (t % (60*60)) * (360 / (60*60))
-    s_degree = (t % 60) * (360 / 60)
-    return h_degree, m_degree, s_degree
-
 def solution(h1, m1, s1, h2, m2, s2):
+    H, M, S = 12*60*60, 60*60, 60
     answer = 0
     begin_sec = h1*60*60 + m1*60 + s1
     end_sec = h2*60*60 + m2*60 + s2
+
+    def calc_degrees(t):
+        h_degree = (t % H) * (360 / H)
+        m_degree = (t % M) * (360 / M)
+        s_degree = (t % S) * (360 / S)
+        return h_degree, m_degree, s_degree
 
     h1, m1, s1 = calc_degrees(begin_sec)
     if s1 == h1 or s1 == m1:

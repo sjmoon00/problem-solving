eedd3fa022174e4521935e4be2e9c69dc52a9619
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-05-25 15:27:49 +0900

    [Passed] DateTime to DateTime (51ms, 16MB)

--- codetree/trail2/DateTime to DateTime/datetime-to-datetime.py
+++ codetree/trail2/DateTime to DateTime/datetime-to-datetime.py
@@ -2,16 +2,12 @@ a, b, c = map(int, input().split())
 
 # Please write your code here.
 def main():
-    if a < 11:
+    target = a*60*24 + b*60 + c
+    base = 11*60*24 + 11*60 + 11
+    if target < base:
         print(-1)
         return
-    if b < 11:
-        print(-1)
-        return
-    if c < 11:
-        print(-1)
-        return
-    
-    print((a*60*24 + b*60 + c) - (11*60*24 + 11*60 + 11))
+
+    print(target - base)
 
 main()

e9efdeaef9f990ea832d04fe4453779599dbca46
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-05-31 01:07:30 +0900

    [Wrong Answer] 연속되는 수 2 (47ms, 16MB)

---
+++ codetree/trail2/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+answer = 0
+idx = 0
+for i in range(n):
+    if i == 0 or arr[i] != arr[i-1]:
+        answer = max(answer, i - idx)
+        idx = i
+print(answer)

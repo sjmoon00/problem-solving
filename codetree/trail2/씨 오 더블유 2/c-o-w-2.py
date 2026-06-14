09ae8a68e1ab36c6491e95ab790e368f90f5a15c
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-06-15 00:04:42 +0900

    [Passed] 씨 오 더블유 2 (49ms, 16MB)

--- codetree/trail2/씨 오 더블유 2/c-o-w-2.py
+++ codetree/trail2/씨 오 더블유 2/c-o-w-2.py
@@ -1,11 +1,22 @@
 n = int(input())
 S = input()
 
-answer = 0
-for i in range(n - 2):
-    for j in range(i + 1, n - 1):
-        for k in range(j + 1, n):
-            if S[i] + S[j] + S[k] == 'COW':
-                answer += 1
+dp = {
+    'C': [0] * n,
+    'CO': [0] * n,
+    'COW': [0] * n
+}
+dp['C'][0] = 1 if S[0] == 'C' else 0
 
-print(answer)
+for i in range(1, n):
+    dp['C'][i], dp['CO'][i], dp['COW'][i] = dp['C'][i - 1], dp['CO'][i - 1], dp['COW'][i - 1]
+
+    s = S[i]
+    if s == 'C':
+        dp['C'][i] = dp['C'][i - 1] + 1
+    elif s == 'O':
+        dp['CO'][i] += dp['C'][i - 1]
+    else:
+        dp['COW'][i] += dp['CO'][i - 1]
+
+print(dp['COW'][n - 1])

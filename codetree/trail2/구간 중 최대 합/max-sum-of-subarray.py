aea85d317544fe7f3de472e5f923365ebd08c834
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-06-26 17:06:57 +0900

    [Passed] 구간 중 최대 합 (54ms, 17MB)

--- codetree/trail2/구간 중 최대 합/max-sum-of-subarray.py
+++ codetree/trail2/구간 중 최대 합/max-sum-of-subarray.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 answer = 0
-for i in range(n - k):
+for i in range(n - k + 1):
     answer = max(answer, sum(arr[i:i + k]))
 
 print(answer)

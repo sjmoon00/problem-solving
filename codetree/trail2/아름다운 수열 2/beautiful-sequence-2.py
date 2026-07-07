2a1b4e043f38dcdc2a67ed15d7333c7951debaf6
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-07-08 00:45:21 +0900

    [Passed] 아름다운 수열 2 (56ms, 16MB)

--- codetree/trail2/아름다운 수열 2/beautiful-sequence-2.py
+++ codetree/trail2/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,9 +1,8 @@
-from itertools import permutations
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-beautiful = set(permutations(B))
+beautiful = tuple(sorted(B))
 def main():
     if N < M:
         print(0)
@@ -11,8 +10,8 @@ def main():
     
     answer = 0
     for i in range(N - M + 1):
-        part = tuple(A[i:i + M])
-        if part in beautiful:
+        part = tuple(sorted(A[i:i + M]))
+        if part == beautiful:
             answer += 1
     
     print(answer)

16a8e6873e290a0df587f19937c74d52673d217a
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-07-19 01:14:17 +0900

    [Passed] 개발자의 능력 2 (56ms, 16MB)

--- codetree/trail2/개발자의 능력 2/ability-of-developer-2.py
+++ codetree/trail2/개발자의 능력 2/ability-of-developer-2.py
@@ -1,10 +1,16 @@
-from itertools import permutations
+from itertools import combinations
 ability = list(map(int, input().split()))
 
+total_idx = set(range(6))
 answer = 1e9
-for teams in permutations(ability):
-    t1, t2, t3 = sum(teams[:2]), sum(teams[2: 4]), sum(teams[4: 6])
-    best, worst = max(t1, t2, t3), min(t1, t2, t3)
-    answer = min(answer, best - worst)
+for team1 in combinations(total_idx, 2):
+    remain_idx = total_idx - set(team1)
+    
+    for team2 in combinations(remain_idx, 2):
+        team3 = remain_idx - set(team2)
+
+        t1, t2, t3 = sum(ability[i] for i in team1), sum(ability[i] for i in team2), sum(ability[i] for i in team3)
+        best, worst = max(t1, t2, t3), min(t1, t2, t3)
+        answer = min(answer, best - worst)
 
 print(answer)

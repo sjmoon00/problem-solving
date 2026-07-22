5c292e91d34ef61d199737a21f52c2fca493b1c8
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-07-22 15:28:06 +0900

    [Passed] 삼각형 만들기 (125ms, 19MB)

--- codetree/trail2/삼각형 만들기/create-triangle.py
+++ codetree/trail2/삼각형 만들기/create-triangle.py
@@ -1,43 +1,23 @@
 from itertools import combinations
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
-INF = float('inf')
 
 answer = 0
 for p1, p2, p3 in combinations(points, 3):
-    slope1 = (p1[1] - p2[1]) / (p1[0] - p2[0]) if p1[0] - p2[0] != 0 else INF
-    slope2 = (p2[1] - p3[1]) / (p2[0] - p3[0]) if p2[0] - p3[0] != 0 else INF
-    slope3 = (p1[1] - p3[1]) / (p1[0] - p3[0]) if p1[0] - p3[0] != 0 else INF
+    pts = [p1, p2, p3]
 
-    if not{0, INF}.issubset([slope1, slope2, slope3]):
-        continue
-    
-    if slope1 not in [0, INF]:
-        horiz, verti = 0, 0
-        if p2[0] == p3[0]:
-            horiz = abs(p1[0] - p3[0])
-            verti = abs(p3[1] - p2[1])
-        elif p1[0] == p3[0]:
-            horiz = abs(p3[0] - p2[0])
-            verti = abs(p1[1] - p3[1])
-        answer = max(answer, horiz * verti)
-    elif slope2 not in [0, INF]:
-        horiz, verti = 0, 0
-        if p1[0] == p2[0]:
-            horiz = abs(p1[0] - p3[0])
-            verti = abs(p1[1] - p2[1])
-        elif p1[0] == p3[0]:
-            horiz = abs(p1[0] - p2[0])
-            verti = abs(p1[1] - p3[1])
-        answer = max(answer, horiz * verti)
-    elif slope3 not in [0, INF]:
-        horiz, verti = 0, 0
-        if p1[0] == p2[0]:
-            horiz = abs(p2[0] - p3[0])
-            verti = abs(p1[1] - p2[1])
-        elif p2[0] == p3[0]:
-            horiz = abs(p1[0] - p2[0])
-            verti = abs(p2[1] - p3[1])
-        answer = max(answer, horiz * verti)
+    for i in range(3):
+        corner = pts[i]
+        other1 = pts[(i + 1) % 3]
+        other2 = pts[(i + 2) % 3]
+
+        if corner[0] == other1[0] and corner[1] == other2[1]:
+            height = abs(corner[1] - other1[1])
+            width = abs(corner[0] - other2[0])
+            answer = max(answer, height * width)
+        elif corner[0] == other2[0] and corner[1] == other1[1]:
+            height = abs(corner[1] - other2[1])
+            width = abs(corner[0] - other1[0])
+            answer = max(answer, width * height)
 
 print(answer)

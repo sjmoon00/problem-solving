6d3415e41181682b65040576fcc3d47aa25c58eb
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-07-22 15:58:40 +0900

    [Passed] 운행 되고 있는 시간 (61ms, 17MB)

--- codetree/trail2/운행 되고 있는 시간/hours-in-service.py
+++ codetree/trail2/운행 되고 있는 시간/hours-in-service.py
@@ -1,20 +1,18 @@
-from collections import defaultdict
 n = int(input())
 times = [tuple(map(int, input().split())) for _ in range(n)]
 a = [t[0] for t in times]
 b = [t[1] for t in times]
 
-arr = defaultdict(int)
+arr = [0] * 1001
 for start, end in times:
     for i in range(start, end):
         arr[i] += 1
 
+total_time = sum(1 for x in arr if x > 0)
 answer = 0
 for start, end in times:
-    t = arr.copy()
-    for i in range(start, end):
-        t[i] -= 1
+    alone_time = sum(1 for x in arr[start:end] if x == 1)
     
-    answer = max(answer, len([x for x in t.values() if x != 0]))
+    answer = max(answer, total_time - alone_time)
 
 print(answer)

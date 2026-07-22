1ca2f02767f818da9433811964944a836ab94a00
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-07-22 16:26:38 +0900

    [Passed] 겹치지 않는 선분 2 (64ms, 16MB)

--- codetree/trail2/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ codetree/trail2/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,6 +1,5 @@
 n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
-lines.sort()
 
 overlapped = [False] * n
 for i in range(n):
@@ -8,7 +7,7 @@ for i in range(n):
     for j in range(i):
         b1, b2 = lines[j][0], lines[j][1]
 
-        if b2 >= a2:
+        if (a1 < b1 and a2 > b2) or (a1 > b1 and a2 < b2):
             overlapped[j] = True
             overlapped[i] = True
 

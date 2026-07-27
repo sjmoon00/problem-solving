cdba5a254fc4b7860d2661fd26a0f5f858797fd8
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-07-28 02:00:05 +0900

    [Passed] 개발자의 순위 (66ms, 17MB)

--- codetree/trail2/개발자의 순위/developers-rank.py
+++ codetree/trail2/개발자의 순위/developers-rank.py
@@ -6,12 +6,12 @@ ranks = defaultdict(set)
 for i in range(k):
     for j in range(n):
         first = arr[i][j]
-        for k in range(j + 1, n):
-            last = arr[i][k]
+        for m in range(j + 1, n):
+            last = arr[i][m]
             ranks[i].add((first, last))
 
 candidates = ranks[0]
-for i in range(1, k):
+for i in range(k):
     candidates = candidates & ranks[i]
 
 print(len(candidates))

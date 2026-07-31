00477a2a6d25bd7c348a15e6e081b04494ab6ba3
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-07-31 17:25:22 +0900

    [Passed] 스승의 은혜 3 (50ms, 16MB)

--- codetree/trail2/스승의 은혜 3/the-grace-form-teacher-3.py
+++ codetree/trail2/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,26 +1,27 @@
 N, B = map(int, input().split())
 gifts = [tuple(map(int, input().split())) for _ in range(N)]
-# P = [gift[0] for gift in gifts]
-# S = [gift[1] for gift in gifts]
 p_s = [sum(x) for x in gifts]
 gifts = [(p_s[i], gifts[i][0], gifts[i][1]) for i in range(N)]
 gifts.sort(key=lambda x: x[0])
 
-answer = -1
+answer = 0
 for i in range(N):
-    total_cost = 0
-    num = 0
+    total, price, ship = gifts[i]
+    cost_i = price // 2 + ship
+    if cost_i > B:
+        continue
+    
+    total_cost = cost_i
+    num = 1
     for j in range(N):
-        total, price, ship = gifts[j]
         if j == i:
-            total_cost += (price//2 + ship)
-        else:
-            total_cost += total
-        num += 1
+            continue
         
-        if total_cost <= B:
-            answer = max(answer, num)
+        if total_cost + gifts[j][0] <= B:
+            total_cost += gifts[j][0]
+            num += 1
         else:
             break
+    answer = max(answer, num)
 
 print(answer)

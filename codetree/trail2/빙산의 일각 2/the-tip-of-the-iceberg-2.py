99668fdfb07313c6a28775d6e2913d6647a18275
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-08-02 03:02:54 +0900

    [Passed] 빙산의 일각 2 (69ms, 17MB)

--- codetree/trail2/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ codetree/trail2/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -4,21 +4,14 @@ H = [int(input()) for _ in range(n)]
 answer = 0
 for level in range(min(H), max(H) + 1):
     arr = [x - level for x in H]
-    start, end = 0, 0
-    for i in range(n):
-        if arr[i] > 0:
-            start = i
-            break
-    for i in range(n - 1, -1, -1):
-        if arr[i] > 0:
-            end = i
-            break
 
-    zero = 0
-    for i in range(start, end + 1):
-        if arr[i] <= 0 and arr[i-1 if i-1 >= 0 else 0] > 0:
-            zero += 1
+    ice = 0
+    if arr[0] > 0:
+        ice += 1
+    for i in range(1, n):
+        if arr[i] > 0 and arr[i-1] <= 0:
+            ice += 1
     
-    answer = max(answer, zero + 1)
+    answer = max(answer, ice)
 
 print(answer)

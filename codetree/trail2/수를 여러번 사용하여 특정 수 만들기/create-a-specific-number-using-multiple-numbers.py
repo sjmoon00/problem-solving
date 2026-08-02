8058fb01e41f2222bdc509e73731ba0b8e4d6c4a
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-08-03 01:16:16 +0900

    [Passed] 수를 여러번 사용하여 특정 수 만들기 (48ms, 16MB)

--- codetree/trail2/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ codetree/trail2/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -1,15 +1,14 @@
 A, B, C = map(int, input().split())
 
 answer = 0
-for x in range(1001):
+# 1. 1001 대신 C // A + 1로 정확한 범위를 지정
+for x in range(C // A + 1):
     a = A * x
-    if a > C:
-        break
-    for y in range(1001):
-        num = a + B * y
-        if num <= C:
-            answer = max(answer, num)
-        else:
-            break
+    
+    # 2. y를 0부터 1씩 올리는 루프 대신, 남은 공간에 들어갈 수 있는 B의 최대 개수를 계산
+    y = (C - a) // B
+    num = a + B * y
+    
+    answer = max(answer, num)
 
 print(answer)

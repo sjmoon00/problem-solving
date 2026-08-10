65b04079a97aa798b2c3dd4cfe0199b9bf6e98ba
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-08-11 01:01:58 +0900

    [Passed] 수들의 최대 차 (114ms, 17MB)

--- codetree/trail2/수들의 최대 차/maximum-difference-in-numbers.py
+++ codetree/trail2/수들의 최대 차/maximum-difference-in-numbers.py
@@ -3,7 +3,7 @@ arr = [int(input()) for _ in range(N)]
 
 min_elem, max_elem = min(arr), max(arr)
 answer = 0
-for begin in range(min_elem, max_elem - K + 1):
+for begin in range(min_elem, max_elem + 1):
     end = begin + K
     cnt = 0
     for x in arr:

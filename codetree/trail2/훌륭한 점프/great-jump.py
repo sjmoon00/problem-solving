d2797ad305d8c7ab08e84b21199648c79e73d016
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-08-11 01:40:32 +0900

    [Wrong Answer] 훌륭한 점프 (47ms, 16MB)

---
+++ codetree/trail2/훌륭한 점프/great-jump.py
@@ -0,0 +1,25 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+def is_possible(max_val):
+    avail_indices = []
+    for i, x in enumerate(arr):
+        if x <= max_val:
+            avail_indices.append(i)
+    
+    l = len(avail_indices)
+    for i in range(l - 1):
+        distance = avail_indices[i+1] - avail_indices[i]
+        if distance > k:
+            return False
+    return True
+
+max_elem = max(arr)
+answer = 1e9
+for max_val in range(max_elem, -1, -1):
+    if is_possible(max_val):
+        answer = max_val
+    else:
+        break
+
+print(answer)

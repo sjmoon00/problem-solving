5ffc214aa7077444d3031e929722eb2935538ac8
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-08-20 22:25:08 +0900

    [Passed] 이상한 폭탄 3 (64ms, 17MB)

--- codetree/trail2/이상한 폭탄 3/strange-bomb-3.py
+++ codetree/trail2/이상한 폭탄 3/strange-bomb-3.py
@@ -6,11 +6,12 @@ d = defaultdict(int)
 for i in range(N):
     b1 = num[i]
     for j in range(max(0, i - K), min(N, i + K + 1)):
+        if i == j: continue
         b2 = num[j]
         if b1 == b2:
             d[b1] += 1
             break
-    
+
 if not d:
     print(0)
 else:

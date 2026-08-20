b8012dee88e7d432077e6b937d2ce6995cbb7d3f
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-08-20 21:54:19 +0900

    [Runtime Error] 초기 수열 복원하기 (79ms, 18MB)

--- codetree/trail2/초기 수열 복원하기/restore-initial-sequence.py
+++ codetree/trail2/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,15 +1,23 @@
-from itertools import permutations
 n = int(input())
 adjacent = list(map(int, input().split())) if n >= 2 else []
 
 def main():
     if n == 1:
-        print(adjacent)
+        print(*adjacent)
         return
     
-    for arr in permutations(range(1, n+1), n):
-        adj = [arr[i]+arr[i+1] for i in range(n-1)]
-        if adj == adjacent:
+    for a1 in range(1, n+1):
+        arr = [a1]
+        visited = [False] * (n + 1)
+        visited[a1] = True
+        for i in range(n-1):
+            s = adjacent[i]
+            a_i1 = s - arr[i]
+            if visited[a_i1]:
+                break
+            visited[a_i1] = True
+            arr.append(a_i1)
+        else:
             print(*arr)
             return
 

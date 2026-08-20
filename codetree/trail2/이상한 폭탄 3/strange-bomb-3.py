0c8bc2329cadf988e880de5602cb5e661f4acc14
sjmoon00 <68841095+sjmoon00@users.noreply.github.com>
2026-08-20 22:23:00 +0900

    [Wrong Answer] 이상한 폭탄 3 (55ms, 17MB)

---
+++ codetree/trail2/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+
+d = defaultdict(int)
+for i in range(N):
+    b1 = num[i]
+    for j in range(max(0, i - K), min(N, i + K + 1)):
+        b2 = num[j]
+        if b1 == b2:
+            d[b1] += 1
+            break
+    
+if not d:
+    print(0)
+else:
+    t = sorted(d.items(), key= lambda x: (x[1], x[0]))
+    print(t[-1][0])
